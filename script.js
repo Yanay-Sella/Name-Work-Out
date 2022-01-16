ce35d0e1b71f26c0a2694c3deb9fb3f149c52e06
o6mer <o6merliraz@gmail.com>
2022-01-17 00:24:48 +0200

    all sets same button fix and working ex number >:)

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 "use strict";
 
-// let addFirstWorkoutBtn = document.querySelector(".add_first_workout_btn");
-// let workoutName = document.querySelector(".workout_name");
-// let workoutBody = document.querySelector(".workout_body");
-// let workoutNameP = document.querySelector(".workout_name_p");
-// let set = document.querySelector(".set");
-// let workoutBodyMain = document.querySelector(".workout_body_main");
-// let workoutBodyEnd = document.querySelector(".workout_body_end");
-// let exreciseNameBtn = document.querySelector(".exrecise_name_btn");
-// let addSetBtn = document.querySelector(".add_set_btn");
-// let allSetsSame = document.querySelector(".all_sets_same");
-// let addFirstSetBtn = document.querySelector(".first_add_set_btn");
-// let addExreciseBtn = document.querySelector(".add_exercise_btn");
-
 let allWorkouts = [];
 const setHtml = `
           <div class="set">
@@ -41,7 +28,7 @@ const exreciseHtml = `
             <button class="add_set_btn">+</button>
             <label class="switch">
               all sets the same
-              <input class="all_sets_same" type="checkbox" checked=true/>
+              <input class="all_sets_same" type="checkbox" />
               <span class="slider round"></span>
             </label>
           </div>
@@ -118,7 +105,12 @@ function createWorkout(el) {
 }
 
 function createExercise() {
-  let workoutBody = this.closest(".workout").querySelector(".workout_body");
+  let workoutBody = this.closest(".workout_body");
+  if (!workoutBody) {
+    workoutBody = this.closest(".workout").querySelector(".workout_body");
+  }
+
+  //   let workoutBody = this.closest(".workout").querySelector(".workout_body");
   workoutBody.insertAdjacentHTML("beforeend", exreciseHtml);
 
   workoutBody.querySelectorAll(".add_set_btn").forEach((btn) => {
@@ -133,12 +125,13 @@ function createExercise() {
     btn.addEventListener("click", setupAllSetsSameBtn);
   });
 
-  console.log("ex");
   // setupAllSetsSameBtn.call(workoutBody.querySelector(".switch"));
 
   let curExerciseDiv = [...workoutBody.querySelectorAll(".exercise")].slice(
     -1
   )[0];
+
+  setExerciseNumber(curExerciseDiv);
 
   allWorkouts
     .find((el) => el.div === this.closest(".workout"))
@@ -181,7 +174,6 @@ function getCurValue(ebm, val, type = ".set") {
 }
 
 function setupAllSetsSameBtn() {
-  console.log("hi");
   const exerciseBodyMain = this.closest(".exercise").querySelector(
     ".exercise_body_main"
   );
@@ -193,9 +185,7 @@ function setupAllSetsSameBtn() {
 
   if (btn.checked) {
     exerciseBodyMain.querySelectorAll(".set").forEach((set, i, arr) => {
-      console.log(arr);
       if (i >= 1) {
-        console.log("hi");
         set.remove();
         set.querySelector(".sets").classList.remove("hidden");
       } else if (arr.length === 0) createSet();
@@ -205,153 +195,21 @@ function setupAllSetsSameBtn() {
     addSetBtn.disabled = true;
     addSetBtn.classList.add("hidden");
     // set.querySelector(".sets").classList.remove("hidden");
-  } else {
+  } else if (!btn.checked) {
+    exerciseBodyMain.querySelectorAll(".set").length === 0
+      ? createSet.call(this)
+      : console.log("not 0 sets");
     exerciseBodyMain.querySelector(".sets").classList.add("hidden");
     addSetBtn.disabled = false;
     addSetBtn.classList.remove("hidden");
   }
 }
 
-// addFirstWorkoutBtn.addEventListener("click", addWorkout.bind(this));
-
-// addSetBtn.addEventListener("click", addSet);
-
-// exreciseNameBtn.addEventListener("click", showWorkoutBody);
-
-// allSetsSame.addEventListener("click", toggleAllSetsTheSame);
-
-// addExreciseBtn.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   this.parentElement.parentElement.insertAdjacentHTML("afterend", exreciseHtml);
-//   createExrecice.call(this);
-//   updateNewExrecise();
-//   updateAddSetBtn();
-// });
-
-// function toggleAllSetsTheSame(e) {
-//   const closestWorkoutBody = this.closest(".workout_body");
-//   if (this.checked) {
-//     closestWorkoutBody.querySelector(".sets").classList.remove("hidden");
-//     closestWorkoutBody
-//       .querySelector(".first_add_set_btn")
-//       .classList.add("hidden");
-//     closestWorkoutBody.querySelector(".first_add_set_btn").disabled = true;
-
-//     closestWorkoutBody.querySelectorAll(".set").forEach((set) => {
-//       if (!set.classList.contains("first_set")) set.remove();
-//       console.log(set);
-//     });
-//   } else {
-//     closestWorkoutBody.querySelector(".first_reps").classList.add("reps");
-//     closestWorkoutBody.querySelector(".first_weight").classList.add("weight");
-//     closestWorkoutBody.querySelector(".first_rest").classList.add("rest");
-
-//     closestWorkoutBody.querySelector(".sets").classList.add("hidden");
-//     closestWorkoutBody
-//       .querySelector(".first_add_set_btn")
-//       .classList.remove("hidden");
-//     closestWorkoutBody.querySelector(".first_add_set_btn").disabled = false;
-//   }
-// }
-
-// function createExrecice() {
-//   let workoutBody = this.closest(".workout_body");
-//   let exreciseName = workoutBody.querySelector(".exrecise_name");
-//   updateNewExrecise();
-//   updateAddSetBtn();
-//   if (exreciseName.value) {
-//     let exName = exreciseName.value;
-//     const ex = new Exrecise(exName, this.closest(".workout_body"));
-//     curExe = ex;
-//     allWorkouts.find((v) => v == curWorkout).addExrecise(curExe);
-//     console.log(workoutBody);
-//     workoutBody.querySelector(".exercise_number").textContent =
-//       allWorkouts.find((v) => v == curWorkout).exrecises.indexOf(curExe) +
-//       1 +
-//       ".";
-//   } else alert("enter exrecise name");
-// }
-
-// function showWorkoutBody() {
-//   workoutBodyMain =
-//     this.closest(".workout_body").querySelector(".workout_body_main");
-//   workoutBodyMain.classList.remove("hidden");
-//   workoutBodyEnd.classList.remove("hidden");
-// }
-
-// function addWorkout(e) {
-//   e.preventDefault();
-//   if (workoutName.value) {
-//     const w = new Workout(workoutName.value);
-//     allWorkouts.push(w);
-//     curWorkout = w;
-
-//     workoutBody.classList.remove("hidden");
-//     workoutName.remove();
-
-//     workoutNameP.classList.remove("hidden");
-//     workoutNameP.textContent = w.name;
-//     createExrecice.bind(this);
-//     showWorkoutBody.bind(this);
-//   } else alert("enter the workout name");
-// }
-
-// function addSet(e) {
-//   e.preventDefault();
-
-//   workoutBodyMain =
-//     this.closest(".workout_body").querySelector(".workout_body_main");
-
-//   const reps = workoutBodyMain.querySelector(".reps");
-//   const weight = workoutBodyMain.querySelector(".weight");
-//   const rest = workoutBodyMain.querySelector(".rest");
-
-//   const repsV = reps.value;
-//   const weightV = weight.value;
-//   const restV = rest.value;
-
-//   const set = {
-//     reps: repsV,
-//     weight: weightV,
-//     rest: restV,
-//     div: this.parentElement,
-//   };
-
-//   allWorkouts
-//     .find((v) => v == curWorkout)
-//     .exrecises.find((ex) => ex === curExe)
-//     .addSet(set);
-
-//   console.log(allWorkouts);
-//   this.parentElement.insertAdjacentHTML("afterend", setHtml);
-//   hiddeAddSetBtn(this);
-
-//   reps.classList.remove("reps");
-//   weight.classList.remove("weight");
-//   rest.classList.remove("rest");
-
-//   updateAddSetBtn();
-// }
-
-// function updateAddSetBtn() {
-//   let curAddSetBtn = document.querySelector(".add_set_btn");
-//   curAddSetBtn.addEventListener("click", addSet);
-//   document
-//     .querySelectorAll(".add_set_btn")
-//     .forEach((btn) => btn.addEventListener("click", addSet));
-// }
-
-// function hiddeAddSetBtn(btn) {
-//   btn.classList.add("hidden");
-//   btn.disabled = true;
-//   btn.classList.remove("add_set_btn");
-// }
-
-// function updateNewExrecise() {
-//   document
-//     .querySelectorAll(".exrecise_name_btn")
-//     .forEach((btn) => btn.addEventListener("click", createExrecice));
-//   document
-//     .querySelectorAll(".add_exercise_btn")
-//     .forEach((btn) => btn.addEventListener("click", createExrecice));
-// }
+function setExerciseNumber(ex) {
+  let numExercises = ex
+    .closest(".workout_body")
+    .querySelectorAll(".exercise").length;
+  console.log(numExercises);
+  console.log(ex);
+  ex.querySelector(".exercise_number").textContent = numExercises + ".";
+}
