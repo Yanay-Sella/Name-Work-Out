b93af156a9b34aeec636af6675ecc9fb3c406ce5
Yanay-Sella <Yanaysella@gmail.com>
2022-01-22 00:53:49 +0200

    fixing modal
    The stone
    working on main page
    also adding a gif of the rock

--- meal.js
+++ meal.js
@@ -1,6 +1,11 @@
+var meals = 0;
+
 $("button").click(function(e){
     if($(this).is("#openModal")){ //if opening a new daily plan
-        addMealPlan();
+        const title = $(".modal-title").text($(".dailyName").val());
+        if(meals === 0){
+            addMealPlan();
+        }
     }
     if($(this).is("#addMeal")){ // if adding a meal
         addMealPlan();
@@ -11,37 +16,47 @@ $("button").click(function(e){
 });
 
 
-
+let dishHtml =`
+    <div class="container-fluid dishDiv input-group mb-3" id="dish">
+        <label for="dishList" class="form-label"></label>
+        <input class="form-control" list="datalistOptions" id="dishList" placeholder="Search dish...">
+        <datalist id="datalistOptions">
+            <option value="Tomato">
+            <option value="Potato">
+            <option value="Apple">
+            <option value="Chicken Breast">
+            <option value="Bread">
+        </datalist>
+        <input type="number" class="dish-amount form-control" placeholder="0.00">
+        <input class="form-control" list="unitDataList" id="unitList" placeholder="unit">
+        <datalist id="unitDataList">
+            <option value="kg">
+            <option value="grams">
+            <option value="ml">
+            <option value="liters">
+            <option value="micrograms">
+        </datalist>
+    </div>
+`;
 let mealHtml = `
     <div class="container-fluid mealDiv">
+        <input type="text" class="form-control mealName" placeholder="meal name" style="border: none">
         <button class="btn btn-primary add-dish">add dish</button>
+        ${dishHtml}
     </div>
-`;
-
-let dishHtml =`
-    <div class="container-fluid dishDiv" id="dish">
-    <label for="dishList" class="form-label"></label>
-    <input class="form-control" list="datalistOptions" id="dishList" placeholder="Type to search...">
-    <datalist id="datalistOptions">
-        <option value="Tomato">
-        <option value="Potato">
-        <option value="Apple">
-        <option value="Chicken Breast">
-        <option value="Bread">
-    </datalist>
-    </div>
-`;
+    `;
 
 function addMealPlan(){
-    $(".modal-body").prepend(mealHtml);
-
-    $(".add-dish").click(function(e){
+    meals++;
+    const meal = $(mealHtml);
+    meal.find(".mealName").val(`meal ${meals}`)
+    $(".mealsContainer").append(meal);
+    $(".add-dish").off("click").click(function(){ //detecting a click for one button!
         addDish(this);
     });
 }
 function addDish(button){
-    // $(this).append(dishHtml);
-    // this.closest(".mealDiv").append(dishDiv);
+
     button.closest(".mealDiv").insertAdjacentHTML("beforeend",dishHtml); // adding the dishDiv html
 }
 
