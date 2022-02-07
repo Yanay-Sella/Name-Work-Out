7dee1dd2c9ffa6b4dfbafa26df37d407a3498ad0
o6mer <o6merliraz@gmail.com>
2022-02-07 20:24:54 +0200

    better loading and bug fixes

--- meal plan/meal modules/foodMacros.js
+++ meal plan/meal modules/foodMacros.js
@@ -13,34 +13,17 @@ export async function findFood() {
 
   console.log(searchFinds);
 
+  const foodIndex = 0;
   const foodsDes = cleanArray(searchFinds.foods.map((f) => f.description));
   foodsDes.forEach((f, i) => {
     const foodDiv = $(
-      `<li><a class="dropdown-item" href="#" data-fdcid=${searchFinds.foods[i].fdcId}>${f}</a></li>`
+      `<li><a class="dropdown-item" href="#" data-index=${i}>${f}</a></li>`
     );
     dropdownMenu.append(foodDiv);
     foodDiv.click((e) => {
-      selectFood(e, nameInput);
+      selectFood(e, nameInput, searchFinds.foods);
     });
   });
-}
-
-const API_KEY = "b1oBKkfsRfpbbbIZFfVrN7m5q9jhK6mdvyL4010F";
-
-async function getFoodByName(des) {
-  const req = await fetch(
-    `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${API_KEY}&query=${des}`
-  );
-  const data = await req.json();
-  return data;
-}
-
-async function getFoodByfdcId(id) {
-  const req = await fetch(
-    `https://api.nal.usda.gov/fdc/v1/food/${id}?api_key=${API_KEY}`
-  );
-  const data = await req.json();
-  return data;
 }
 
 async function getFoods(des) {
@@ -58,26 +41,21 @@ function cleanArray(arr) {
   return [...new Set(arr)];
 }
 
-function selectFood(e, input) {
+function selectFood(e, input, foods) {
   const el = $(e.target);
-  const id = el.data("fdcid");
-  input.attr("data-fdcid", id);
+  const index = el.data("index");
+  input.attr("data-index", index);
   input.val(el.html());
-  renderMacros(el, id);
+  renderMacros(el, foods[index]);
 }
 
-async function renderMacros(el, id) {
+async function renderMacros(el, foodItem) {
   const dishDiv = $(el).closest(".dishDiv");
-
-  if (!id) {
-    console.log("no id");
-    return;
-  }
 
   dishDiv.find(".dropdown-cal").text(`loading...`);
   dishDiv.find(".dropdown-prot").text(`loading...`);
   dishDiv.find(".dropdown-carbs").text(`loading...`);
-  const foodItem = await getFoodByfdcId(id);
+  // const foodItem = await getFoodByfdcId(id);
 
   if (!foodItem) {
     console.log("no food found");
@@ -86,13 +64,11 @@ async function renderMacros(el, id) {
   const macros = getMacrosFromFood(foodItem);
   if (!macros) return;
 
-  // dishDiv.find(".macros-dropdown-menu").toggleClass("show");
   dishDiv.find(".dropdown-cal").text(`Calories: ${macros.calories}`);
   dishDiv.find(".dropdown-prot").text(`Protein: ${macros.protien}`);
   dishDiv.find(".dropdown-carbs").text(`Carbs: ${macros.carbs}`);
 
   console.log(foodItem);
-  //labelNutrients
 }
 
 function getMacrosFromFood(food) {
@@ -105,8 +81,26 @@ function getMacrosFromFood(food) {
     return;
   }
   return {
-    calories: macros.find((n) => n.nutrient.name == "Energy").amount ?? 0,
-    carbs: macros.find((n) => n.nutrient.name == "Carbohydrates")?.amount ?? 0,
-    protien: macros.find((n) => n.nutrient.name == "Protein").amount ?? 0,
+    calories: macros.find((n) => n.nutrientName == "Energy").value ?? 0,
+    carbs: macros.find((n) => n.nutrientName == "Carbohydrates")?.value ?? 0,
+    protien: macros.find((n) => n.nutrientName == "Protein").value ?? 0,
   };
 }
+
+const API_KEY = "b1oBKkfsRfpbbbIZFfVrN7m5q9jhK6mdvyL4010F";
+
+async function getFoodByName(des) {
+  const req = await fetch(
+    `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${API_KEY}&query=${des}`
+  );
+  const data = await req.json();
+  return data;
+}
+
+// async function getFoodByfdcId(id) {
+//   const req = await fetch(
+//     `https://api.nal.usda.gov/fdc/v1/food/${id}?api_key=${API_KEY}`
+//   );
+//   const data = await req.json();
+//   return data;
+// }
